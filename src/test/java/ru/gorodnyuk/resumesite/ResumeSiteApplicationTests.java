09e35bbe1805d3db09ebece3f36aa76536af6d88
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-10-05 11:42:33 +0300

    Init commit

---
+++ src/test/java/ru/gorodnyuk/resumesite/ResumeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gorodnyuk.resumesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
