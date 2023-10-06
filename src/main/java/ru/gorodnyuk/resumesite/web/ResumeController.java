12d8694b0a3f120bd3b456c6eddf3e34f28611a9
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-10-06 10:11:44 +0300

    Удалил лишнее, добавил статровую страницу

---
+++ src/main/java/ru/gorodnyuk/resumesite/web/ResumeController.java
@@ -0,0 +1,13 @@
+package ru.gorodnyuk.resumesite.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResumeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
