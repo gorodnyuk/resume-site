2dc452c9de67a23396341114435ff03b9de430e4
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-11-10 19:03:26 +0300

    Добавлена проверка входа с мобильных устройств

--- src/main/java/ru/gorodnyuk/resumesite/web/ResumeController.java
+++ src/main/java/ru/gorodnyuk/resumesite/web/ResumeController.java
@@ -2,8 +2,10 @@ package ru.gorodnyuk.resumesite.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import ru.gorodnyuk.resumesite.service.StatisticsService;
 
 @Controller
@@ -13,23 +15,39 @@ public class ResumeController {
     private final StatisticsService statisticsService;
 
     @GetMapping("/")
-    public String getIndexPage(HttpServletRequest httpServletRequest) {
+    public String getIndexPage(@RequestHeader("User-Agent") String userAgent, HttpServletRequest httpServletRequest) {
+        if (StringUtils.containsIgnoreCase(userAgent, "Mobi")
+                || StringUtils.containsIgnoreCase(userAgent, "Android")) {
+            return "mobile-error.html";
+        }
         statisticsService.calculate(httpServletRequest.getRemoteAddr());
         return "index.html";
     }
 
     @GetMapping("/work")
-    public String getWorkInfoPage() {
+    public String getWorkInfoPage(@RequestHeader("User-Agent") String userAgent) {
+        if (StringUtils.containsIgnoreCase(userAgent, "Mobi")
+                || StringUtils.containsIgnoreCase(userAgent, "Android")) {
+            return "mobile-error.html";
+        }
         return "work.html";
     }
 
     @GetMapping("/projects")
-    public String getProjectsInfoPage() {
+    public String getProjectsInfoPage(@RequestHeader("User-Agent") String userAgent) {
+        if (StringUtils.containsIgnoreCase(userAgent, "Mobi")
+                || StringUtils.containsIgnoreCase(userAgent, "Android")) {
+            return "mobile-error.html";
+        }
         return "projects.html";
     }
 
     @GetMapping("/about")
-    public String getAboutInfoPage() {
+    public String getAboutInfoPage(@RequestHeader("User-Agent") String userAgent) {
+        if (StringUtils.containsIgnoreCase(userAgent, "Mobi")
+                || StringUtils.containsIgnoreCase(userAgent, "Android")) {
+            return "mobile-error.html";
+        }
         return "about.html";
     }
 }
